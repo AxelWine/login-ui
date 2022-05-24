7a1386bc106e961e482bdaa358cf8ba28ab4bb38
axelwine <lalescontacto@gmail.com>
2022-05-24 12:21:34 +0200

    Mejorar el header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,11 @@ import "./Header.css";
 function Header() {
     return (
         <>
+            <Navbar className="header-navbar header">
+                <Container>
+                    <Navbar.Brand href="#">Axel Corporation</Navbar.Brand>
+                </Container>
+            </Navbar>
             <Navbar className="header-navbar" bg="light">
                 <Container className="d-flex header-container">
                     Iniciar sesión
